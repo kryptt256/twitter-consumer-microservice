c3e1274b933210feaf7098e5ae72e9f5e3364b27
vvmaster <vvmaster@gmail.com>
2020-02-27 23:26:19 +0100

    refactor name

--- src/main/java/com/challenge/twitterconsumer/TwitterConsumerApplication.java
+++ src/main/java/com/challenge/twitterconsumer/TwitterConsumerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterconsumerApplication {
+public class TwitterConsumerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TwitterconsumerApplication.class, args);
+		SpringApplication.run(TwitterConsumerApplication.class, args);
 	}
 
 }
