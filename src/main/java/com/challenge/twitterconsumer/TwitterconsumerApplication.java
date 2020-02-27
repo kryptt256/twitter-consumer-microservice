f083a1ce2b186be0e304cff25d60539c92c9f647
vvmaster <vvmaster@gmail.com>
2020-02-27 21:09:58 +0100

    initial commit

---
+++ src/main/java/com/challenge/twitterconsumer/TwitterconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.twitterconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterconsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/twitterconsumer/TwitterconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.twitterconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
